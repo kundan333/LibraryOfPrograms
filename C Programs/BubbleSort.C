19b327e0660f06b8f58b37ab6748e68b0bf63028
kundan <kundanbora77@gmail.com>
2017-09-10 20:13:38 -1100

    Added Bubble Sort Program

---
+++ C Programs/BubbleSort.C
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<conio.h>
+#define N 5
+void main()
+{
+void bubble(int *a);
+int a[N],i;
+clrscr();
+printf("Enter Elements in array\n");
+for(i=0;i<N;i++)
+{
+printf("Enter Value of A[%d]:",i);
+scanf("%d",&a[i]);
+}
+bubble(a);
+printf("Sorted List\n");
+for(i=0;i<N;i++)
+{
+printf("A[%d]=%d\n",i,a[i]);
+}
+getch();
+}
+void bubble(int *a)
+{
+int i,j,temp;
+for(i=0;i<N-1;i++)
+{
+for(j=0;j<N-i-1;j++)
+{
+if(a[j]>a[j+1])
+{
+temp=a[j];
+a[j]=a[j+1];
+a[j+1]=temp;
+}
+}
+}
+}
