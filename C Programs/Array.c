f156f310421e77616cb031eea75e691be7486f9f
kundan <kundanbora77@gmail.com>
2017-09-11 21:15:22 -1100

    Added Array Program

---
+++ C Programs/Array.c
@@ -0,0 +1,26 @@
+#include<conio.h>
+#include<stdio.h>
+
+void main()
+{
+
+int a[100],n,i;
+//a[100] -- 100 is length of array
+clrscr();
+
+printf("Enter length of array \n");
+scanf("%d",&n);
+printf("Enter elements to array \n");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("Your array is = \n");
+
+for(i=0;i<n;i++)
+{
+printf(" a[%d] = %d \n",i,a[i]);
+}
+
+getch();
+}
