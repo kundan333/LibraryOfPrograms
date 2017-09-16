b54604a3d5c9016a0945c5bcb87653d6c9b18079
kundan <kundanbora77@gmail.com>
2017-09-15 20:48:47 -1100

    added merge sort program in c

---
+++ C Programs/MergeSort.c
@@ -0,0 +1,121 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<conio.h>
+
+
+
+
+// Merges two subarrays of arr[].
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 =  r - m;
+
+    /* create temp arrays */
+    int* L=(int*)calloc(n1,sizeof(arr[0]));
+
+    int* R=(int*)calloc(n2,sizeof(arr[0]));
+     /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++)
+	L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+	R[j] = arr[m + 1+ j];
+
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = l; // Initial index of merged subarray
+    while (i < n1 && j < n2)
+    {
+	if (L[i] <= R[j])
+	{
+	    arr[k] = L[i];
+	    i++;
+	}
+	else
+	{
+	    arr[k] = R[j];
+	    j++;
+	}
+	k++;
+    }
+
+    /* Copy the remaining elements of L[], if there
+       are any */
+    while (i < n1)
+    {
+	arr[k] = L[i];
+	i++;
+	k++;
+    }
+
+    /* Copy the remaining elements of R[], if there
+       are any */
+    while (j < n2)
+    {
+	arr[k] = R[j];
+	j++;
+	k++;
+    }
+}
+
+/* l is for left index and r is right index of the
+   sub-array of arr to be sorted */
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+	// Same as (l+r)/2, but avoids overflow for
+	// large l and h
+	int m = l+(r-l)/2;
+
+	// Sort first and second halves
+	mergeSort(arr, l, m);
+	mergeSort(arr, m+1, r);
+
+	merge(arr, l, m, r);
+    }
+}
+
+
+void printArray(int A[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+	{
+	printf("%d ", A[i]);
+    printf("\n");
+	}
+}
+
+
+void main()
+{
+int i,n,arr[100];
+
+ printf("Enter the length of array");
+ scanf("%d",&n);
+
+
+ printf(" enter the elements of array ");
+
+ for(i=0;i<n-1;i++)
+ {
+ scanf(" %d ",&arr[i]);
+ }
+
+ printf("Given array is \n");
+
+
+ mergeSort(arr, 0, n - 1);
+
+ printf("\nSorted array is \n");
+ printArray(arr, n);
+
+getch();
+
+}
+
