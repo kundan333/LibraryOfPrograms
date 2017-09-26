aa2a8d56e956d5961d4980190b23b969594e7ea4
kundan <kundanbora77@gmail.com>
2017-09-25 18:54:18 -1100

    Find Area of circle class

---
+++ Java Programs/FindCircleArea/Circle.java
@@ -0,0 +1,12 @@
+public class Circle{
+
+double area;
+double radius;
+public void circleArea(double radius){
+double r =radius;
+ area = 3.14 *r*r;
+System.out.println(area);
+}
+
+
+}
---
+++ Java Programs/FindCircleArea/FindArea.java
@@ -0,0 +1,16 @@
+class FindArea{
+
+public static void main(String arg[]){
+
+Circle c = new Circle();
+// c is object of circle class
+ c.circleArea(8);
+//circleArea is a method of circle class
+//we are passing 8 as parameter 
+c.circleArea(9);
+}
+
+
+
+
+}
