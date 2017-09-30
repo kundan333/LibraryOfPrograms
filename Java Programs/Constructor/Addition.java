016d9db6e5e8d2f41545fc95c80def10d0c99ebb
kavita <kavitabhatt737@gmail.com>
2017-09-30 10:14:19 +0530

    added parameterized constructor program

---
+++ Java Programs/Constructor/Addition.java
@@ -0,0 +1,17 @@
+class Addition
+{
+Addition( int a,int b)
+{
+int sum;
+sum=a+b;
+System.out.println("addition is: " +sum);
+}
+public static void main(String args[])
+{
+Addition A1=new Addition(20,10);
+}
+}
+
+
+
+
