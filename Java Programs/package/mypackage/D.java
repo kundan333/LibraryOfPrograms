d93df28c65b12ab61d4315868e3163bde012fc64
kundan <kundanbora77@gmail.com>
2017-10-31 00:20:28 -1100

    java package programs added

---
+++ Java Programs/package/mypackage/D.java
@@ -0,0 +1,11 @@
+package mypackage;
+
+class D{
+
+public static void main(String args[])
+{
+S obj = new S("kundan");
+obj.display();
+}
+
+}
---
+++ Java Programs/package/mypackage/S.java
@@ -0,0 +1,14 @@
+package mypackage;
+class S{
+String name;
+
+S(String n){
+name=n;
+}
+
+public void display(){
+System.out.println(name);
+}
+
+
+}
