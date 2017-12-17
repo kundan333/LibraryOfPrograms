6632a7fca986e2ce745108e830f1eb92fa0a8e2a
kundan <kundanbora77@gmail.com>
2017-12-17 22:03:50 +0530

    Added Quick Sort

--- Java Programs/package/mypackage/Ac.java
+++ Java Programs/package/mypackage/Ac.java
@@ -6,13 +6,7 @@ class AccountBalance extends Balance{
 public static void main(String args[])
 {
 Balance obj = new Balance("K.J.Fiending",100);
-//current[1] = new Balance("Will ",150);
-//current[2] = new Balance("John",100);
-
-//for(int i=0;i<3;i++){
 obj.show();
-//}
-
 }
 
 }
--- Java Programs/package/mypackage/Balance.java
+++ Java Programs/package/mypackage/Balance.java
@@ -21,22 +21,3 @@ System.out.println(name + " :  $" + bal);
 }
 
 }
-/*
-class AccountBalance{
-
-public static void main(String args[])
-{
-Balance current[] = new Balance[3];
-
-current[0] = new Balance("K.J.Fiending",100);
-current[1] = new Balance("Will ",150);
-current[2] = new Balance("John",100);
-
-for(int i=0;i<3;i++){
-current[i].show();
-}
-
-}
-
-}
-*/
